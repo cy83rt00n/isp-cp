45b7808c4f3aeb3241c53cf2577793d89d05fb4d
Artem Tsybin <cy83rt00n@gmail.com>
2019-09-25 12:18:38 +0300

    made react build

---
+++ www/react/build/precache-manifest.455fc48322f7a39986930101927a6e1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c766bf57c28c1276bf79ba6a23879d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5c196b0519cb6bba257",
+    "url": "/static/css/main.5a787e69.chunk.css"
+  },
+  {
+    "revision": "fc443460867efa8dc12c",
+    "url": "/static/js/2.de650afc.chunk.js"
+  },
+  {
+    "revision": "f5c196b0519cb6bba257",
+    "url": "/static/js/main.e9ae9e5b.chunk.js"
+  },
+  {
+    "revision": "1bbb76ac8402e0a4adf4",
+    "url": "/static/js/runtime~main.ef2a48d5.js"
+  }
+]);
