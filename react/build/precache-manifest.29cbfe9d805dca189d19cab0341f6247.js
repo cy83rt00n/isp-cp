f55c86cf7156e2b75b345f95e15ff65a4f417e41
Artem Tsybin <cy83rt00n@gmail.com>
2019-10-02 17:49:27 +0300

    prod build

---
+++ react/build/precache-manifest.29cbfe9d805dca189d19cab0341f6247.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "754eb02854eaee374f14ec63a8c32c4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93e7e7632facc39368db",
+    "url": "/static/js/2.f35217c9.chunk.js"
+  },
+  {
+    "revision": "38f7831aff974e1ef315",
+    "url": "/static/js/main.ee2620d1.chunk.js"
+  },
+  {
+    "revision": "0b4554d0d4cc6c60d123",
+    "url": "/static/js/runtime~main.a3f73814.js"
+  }
+]);
