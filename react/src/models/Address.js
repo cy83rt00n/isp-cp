2bf4c154b73f1ca43bfb9fe004083fc6ac040cc8
Artem Tsybin <cy83rt00n@gmail.com>
2019-10-02 17:19:05 +0300

    Fix bug with issue update

---
+++ react/src/models/Address.js
@@ -0,0 +1,16 @@
+let Address = {
+    city : {
+        id:0, title:''
+    },
+    street: {
+        id:0, title:''
+    },
+    home: {
+        id:0, title:''
+    },
+    flat: {
+        id:0, title:''
+    }
+}
+
+export default Address;
